214b71343cf46a82cabb1149c94c274e893450ac
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-04 15:21:06 -0300

    refactor: implement repository pattern

--- src/middlewares/auth/checkIfUserExists.js
+++ src/middlewares/auth/checkIfUserExists.js
@@ -1,14 +1,17 @@
-import connection from "./../../database/postgre.js";
+import { getUserByEmail } from "../../repositories/authRepository.js";
 
 async function checkIfUserExists(req, res, next) {
     const { email } = req.body;
-    const query = "SELECT * FROM users WHERE email = $1;";
-    const { rowCount : user } = await connection.query(query, [email]);
-    const isUserRegistered = user > 0;
-    if(isUserRegistered) {
-        return res.sendStatus(409);
-    }
-    next();
+    try {
+        const { rowCount : user } = await getUserByEmail(email);
+        const isUserRegistered = user > 0;
+        if(isUserRegistered) {
+            return res.sendStatus(409);
+        }
+        next();
+    } catch {
+        res.sendStatus(500);
+    }   
 }
 
 export default checkIfUserExists;
