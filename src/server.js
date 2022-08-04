4b4975b338bbbf5e627952ed3bf6ca6533a8c832
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-04 12:40:45 -0300

    feat: implement server configuration

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes/index.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors(), express.json());
+app.use(router);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("Server running"));
