5f6803c020da21cff60204e6d70028fdd9510e04
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-04 15:20:49 -0300

    feat: implement function to get user by email

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -1,9 +1,15 @@
 import connection from "./../database/postgre.js"
 
+async function getUserByEmail(email) {
+    const query = "SELECT * FROM users WHERE email = $1;";
+    const user = await connection.query(query, [email]);
+    return user;
+}
+
 async function registerUser(user, hash) {
     const { name, email } = user;
     const query = "INSERT INTO users (name, email, password) VALUES ($1, $2, $3);";
     await connection.query(query, [name, email, hash]);
 }
 
-export { registerUser };
+export { getUserByEmail, registerUser };
