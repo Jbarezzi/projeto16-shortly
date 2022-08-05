b7493c64d5ad1b09f59f25585a9f9694c33cb13e
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-05 14:30:14 -0300

    feat: implement url validation

--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import validateWithJoi from "./../middlewares/validateWithJoi";
+import newUrl from "./../schemas/urlSchemas.js";
 
 const urlRouter = Router();
 
-urlRouter.post("/urls/shorten");
+urlRouter.post("/urls/shorten", validateWithJoi(newUrl));
 
 export default urlRouter;
