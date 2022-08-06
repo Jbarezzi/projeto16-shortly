4c6e0b5c5155ac3193342886eca810615a16cb57
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 13:01:41 -0300

    feat: add get short url repository

--- src/repositories/urlRepository.js
+++ src/repositories/urlRepository.js
@@ -2,14 +2,20 @@ import connection from "./../database/postgre.js";
 
 async function registerUrl(newUrl) {
     const { url, shortUrl, userId } = newUrl;
-    const query = 'INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3);';
+    const query = `INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3);`;
     await connection.query(query, [url, shortUrl, userId]);
 }
 
 async function getUrlById(id) {
-    const query = "SELECT * FROM urls WHERE id = $1;";
+    const query = `SELECT * FROM urls WHERE id = $1;`;
     const url = await connection.query(query, [id]);
     return url;
 }
 
-export { registerUrl, getUrlById };
+async function getShortUrl(shortUrl) {
+    const query = `SELECT * FROM urls WHERE "shortUrl" = $1;`;
+    const url = await connection.query(query, [shortUrl]);
+    return url;
+}
+
+export { registerUrl, getUrlById, getShortUrl };
