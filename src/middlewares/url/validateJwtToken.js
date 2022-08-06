ecb352eef9998082b25a9c24aa7864a9b937f363
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 14:28:35 -0300

    refactor

--- src/middlewares/url/validateJwtToken.js
+++ src/middlewares/url/validateJwtToken.js
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 
 function validateJwtToken(req, res, next) {
     const token = req.headers;
-    token.replace("Bearer ", "");
-    const { payload: user } = jwt.decode(token);
+    token?.replace("Bearer ", "");
+    const user = jwt.decode(token);
     res.locals.user = user;
     try {
         jwt.verify(token, process.env.JWT_SECRET_KEY);
