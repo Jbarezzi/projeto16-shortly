f66aab7b21fa8fd31a3354e5689b853030d0dd4e
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 11:29:57 -0300

    feat: add url get route

--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createShortUrl } from "./../controllers/urlController.js";
-import { validateJwtToken, validateWithJoi } from "./../middlewares/index.js";
+import { createShortUrl, getUrl } from "./../controllers/urlController.js";
+import { checkIfUrlExists, validateJwtToken, validateWithJoi } from "./../middlewares/index.js";
 import newUrl from "./../schemas/urlSchemas.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validateJwtToken, validateWithJoi(newUrl), createShortUrl);
-
+urlRouter.get("/urls/:id", checkIfUrlExists, getUrl);
 
 export default urlRouter;
