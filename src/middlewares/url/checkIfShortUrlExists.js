94c09baf7be2b20c8d9a42aca3102a3b81d37cd5
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 13:01:22 -0300

    feat: implement verification if shortUrl exists

---
+++ src/middlewares/url/checkIfShortUrlExists.js
@@ -0,0 +1,13 @@
+import getShortUrl from "./../../repositories/urlRepository.js;"
+
+async function checkIfShortUrlExists(req, res, next) {
+    const shortUrl = req.params.shortUrl;
+    const { rowCount: url } = await getShortUrl(shortUrl);
+    const hasUrl = url === 1;
+    if(hasUrl) {
+        return next();
+    }
+    res.sendStatus(404);
+}
+
+export default checkIfShortUrlExists;
