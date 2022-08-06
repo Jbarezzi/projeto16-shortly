ad70cb94446a39a6270b1fb16e7e7d0ad89826f6
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 14:48:45 -0300

    feat: add function to verificate if user exists by id

---
+++ src/middlewares/auth/checkIfUserExistsById.js
@@ -0,0 +1,17 @@
+import { getUserById } from "../../repositories/authRepository.js";
+
+async function checkIfUserExistsById(req, res, next) {
+    const { id } = res.locals.user;
+    try {
+        const { rowCount : user } = await getUserById(id);
+        const isUserRegistered = user > 0;
+        if(isUserRegistered) {
+            return next();
+        }
+        res.sendStatus(404);
+    } catch {
+        res.sendStatus(500);
+    }   
+}
+
+export default checkIfUserExistsById;
