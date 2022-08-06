06c5f3f9cbc2d3ad5ee9a09117eddccea2559a00
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 11:30:11 -0300

    refactor: centralize export

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,5 +2,6 @@ import validateWithJoi from "./validateWithJoi.js";
 import validateJwtToken from "./url/validateJwtToken.js";
 import validateSignin from "./auth/validateSignin.js";
 import checkIfUserExists from "./auth/checkIfUserExists.js";
+import checkIfUrlExists from "./url/checkIfUrlExists.js";
 
-export { validateWithJoi, validateJwtToken, validateSignin, checkIfUserExists };
+export { validateWithJoi, validateJwtToken, validateSignin, checkIfUserExists, checkIfUrlExists };
