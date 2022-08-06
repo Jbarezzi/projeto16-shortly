6f88fe05141c105207cad52a8a731ae90e3b1ca2
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 14:28:47 -0300

    feat: implemente url delete route

--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { createShortUrl, getUrl, redirectUser } from "./../controllers/urlController.js";
-import { checkIfShortUrlExists, checkIfUrlExists, increaseVisitCount, validateJwtToken, validateWithJoi } from "./../middlewares/index.js";
+import { createShortUrl, getUrl, handleUrlDelete, redirectUser } from "./../controllers/urlController.js";
+import { checkIfShortUrlExists, checkIfUrlExists, checkIfUrlIsFromUser, increaseVisitCount, validateJwtToken, validateWithJoi } from "./../middlewares/index.js";
 import newUrl from "./../schemas/urlSchemas.js";
 
 const urlRouter = Router();
@@ -8,5 +8,6 @@ const urlRouter = Router();
 urlRouter.post("/urls/shorten", validateJwtToken, validateWithJoi(newUrl), createShortUrl);
 urlRouter.get("/urls/:id", checkIfUrlExists, getUrl);
 urlRouter.get("/urls/open/:shortUrl", checkIfShortUrlExists, increaseVisitCount, redirectUser);
+urlRouter.delete("/urls/:id", validateJwtToken, checkIfUrlExists, checkIfUrlIsFromUser, handleUrlDelete);
 
 export default urlRouter;
