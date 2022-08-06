cc0f5e06c27a12ad706b9ef0f5d23804ff52616e
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-06 14:48:11 -0300

    feat: implement user route to show private data

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { signin, signup } from "../controllers/authController.js";
-import { validateSignin, checkIfUserExists, validateWithJoi } from "./../middlewares/index.js";
+import { returnUserData, signin, signup } from "../controllers/authController.js";
+import { validateSignin, checkIfUserExists, validateWithJoi, validateJwtToken, checkIfUserExistsById } from "./../middlewares/index.js";
 import { signinUser, newUser } from "../schemas/authSchemas.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validateWithJoi(newUser), checkIfUserExists, signup);
 authRouter.post("/signin", validateWithJoi(signinUser), validateSignin, signin);
+authRouter.get("/users/me", validateJwtToken, checkIfUserExistsById, returnUserData);
 
 export default authRouter;
